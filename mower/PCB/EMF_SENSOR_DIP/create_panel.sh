1497f8c2805cba0b798e2f50d177fe9525ea0172
ExolutionTech <exolutionza@gmail.com>
2025-10-12 23:19:13 +0200

    .

--- mower/PCB/EMF_SENSOR_DIP/create_panel.sh
+++ mower/PCB/EMF_SENSOR_DIP/create_panel.sh
@@ -1,3 +1,3 @@
 kikit panelize \
     --layout 'hspace: 1mm; vspace: 1mm; rows: 4; cols: 6' \
-    '/home/imran/Documents/botserv-mono/mowbot/PCB/EMF_SENSOR_DIP/EMF_SENSOR.kicad_pcb' '/home/imran/Documents/botserv-mono/mowbot/PCB/EMF_SENSOR_DIP/EMF_SENSOR_PANEL.kicad_pcb'
+    '/home/imran/Documents/botkorp-mono/mowbot/PCB/EMF_SENSOR_DIP/EMF_SENSOR.kicad_pcb' '/home/imran/Documents/botkorp-mono/mowbot/PCB/EMF_SENSOR_DIP/EMF_SENSOR_PANEL.kicad_pcb'
