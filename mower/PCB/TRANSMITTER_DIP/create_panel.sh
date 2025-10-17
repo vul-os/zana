c9ad43cc2b9d79355bc8bebb1781e5f22e055359
ExolutionTech <exolutionza@gmail.com>
2025-10-17 16:08:56 +0200

    .

---
+++ mower/PCB/TRANSMITTER_DIP/create_panel.sh
@@ -0,0 +1,3 @@
+kikit panelize \
+    --layout 'hspace: 1mm; vspace: 0.5mm; rows: 4; cols: 4' \
+    '/home/imran/Documents/botkorp-mono/mowbot/PCB/TRANSMITTER_DIP/TRANSMITTER.kicad_pcb' '/home/imran/Documents/botkorp-mono/mowbot/PCB/TRANSMITTER_DIP/TRANSMITTER_PANEL.kicad_pcb'
